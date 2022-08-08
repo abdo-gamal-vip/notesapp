696f2e7560292a41cdc36c5abf26098684965f63
abdo-gamal-vip <abdo.gamal.vip@gmail.com>
2022-08-08 16:36:38 +0200

    modified:   lib/app/auth/login.dart

--- lib/app/auth/login.dart
+++ lib/app/auth/login.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -6,16 +7,25 @@ import 'package:notesapp/app/auth/signup.dart';
 import 'package:notesapp/app/home.dart';
 import 'package:notesapp/components/crud.dart';
 import 'package:notesapp/constant/linkapi.dart';
-
 import '../../components/customtextform.dart';
 import '../../components/small_button.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   TextEditingController email = TextEditingController();
+
   TextEditingController password = TextEditingController();
+
   GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
   Crud crud = Crud();
+
   login() async {
     if (formkey.currentState!.validate()) {
       try {
@@ -24,9 +34,16 @@ class Login extends StatelessWidget {
           "password": password.text,
         });
         if (response['status'] == "success") {
-          return navigator!.pushReplacementNamed("home");
+          Navigator.of(context)
+              .pushNamedAndRemoveUntil("home", (route) => false);
         } else {
-          print("login failled");
+          setState(() {
+            AwesomeDialog(
+              context: context,
+              title: "تنبيه ",
+              body: Text("البريد الالكترونى خطأ او كلمه المرور"),
+            ).show();
+          });
         }
       } catch (e) {
         print("$e error e");
@@ -56,8 +73,8 @@ class Login extends StatelessWidget {
                   CustomTextForm(
                     valid: (val) {},
                     mycontroller: email,
-                    hinttype: 'Type Your Username',
-                    iname: Icon(Icons.person),
+                    hinttype: 'Type Your Email',
+                    iname: Icon(Icons.email_outlined),
                   ),
                   SizedBox(
                     height: 8,
@@ -65,8 +82,8 @@ class Login extends StatelessWidget {
                   CustomTextForm(
                     valid: (val) {},
                     mycontroller: password,
-                    hinttype: 'Type Your Email',
-                    iname: Icon(Icons.email_outlined),
+                    hinttype: 'Type Your password',
+                    iname: Icon(Icons.vpn_key),
                   ),
                   SizedBox(
                     height: 8,
