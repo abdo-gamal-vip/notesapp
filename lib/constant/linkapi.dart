ab4843a534055f3ab6764cb61c029b6ca4687f63
abdo-gamal-vip <abdo.gamal.vip@gmail.com>
2022-08-09 19:34:50 +0200

    modified:   lib/constant/linkapi.dart 	modified:   lib/main.dart

--- lib/constant/linkapi.dart
+++ lib/constant/linkapi.dart
@@ -1,5 +1,5 @@
 String linkserverName = "http://127.0.0.1/notesapp";
 
 //auth
-String Signuplink = "http://10.0.2.2/notesapp/auth/signup.php";
-String Loginlink = "http://10.0.2.2/notesapp/auth/login.php";
+const String Signuplink = "http://10.0.2.2/notesapp/auth/signup.php";
+const String constLoginlink = "http://10.0.2.2/notesapp/auth/login.php";
