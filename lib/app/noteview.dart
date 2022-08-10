c974be7402457ca2102f71199901d69ac1b437e5
abdo-gamal-vip <abdo.gamal.vip@gmail.com>
2022-08-10 19:55:24 +0200

    modified:   lib/app/noteview.dart

--- lib/app/noteview.dart
+++ lib/app/noteview.dart
@@ -22,18 +22,20 @@ class _AddViewState extends State<AddView> with Crud {
   TextEditingController content = TextEditingController();
   bool isLoading = false;
   addNote() async {
-    if (_tkey.currentState!.validate() || _ckey.currentState!.validate()) {
-      try {
-        var response = await postRequset(addlink, {
-          "users": shardprefs.getString("u_id"),
-          "title": title.text,
-          "content": content.text,
-        });
-        print(response);
-        return response;
-      } catch (e) {
-        print(e);
-      }
+    isLoading = true;
+    setState(() {});
+    try {
+      var response = await postRequset(addlink, {
+        "users": shardprefs.getString("u_id"),
+        "title": title.text,
+        "content": content.text,
+      });
+      isLoading = false;
+      setState(() {});
+      print(response);
+      return response;
+    } catch (e) {
+      print(e);
     }
   }
 
@@ -44,114 +46,128 @@ class _AddViewState extends State<AddView> with Crud {
           backgroundColor: Colors.teal,
           title: Text("add Note"),
         ),
-        body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          child: Column(children: [
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-                padding: EdgeInsets.all(5),
-                height: 400,
+        body: isLoading == true
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+                height: double.infinity,
                 width: double.infinity,
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: title,
-                      key: _tkey,
-                      maxLines: 1,
-                      decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.teal),
-                              borderRadius: BorderRadius.circular(10)),
-                          focusColor: Colors.teal,
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          border: OutlineInputBorder(),
-                          labelText: "Title",
-                          hintText: "Enter Title",
-                          prefixIcon: Icon(Icons.title),
-                          disabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(color: Colors.teal))),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextField(
-                      controller: content,
-                      key: _ckey,
-                      maxLines: 10,
-                      decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.teal),
-                              borderRadius: BorderRadius.circular(10)),
-                          focusColor: Colors.teal,
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          border: OutlineInputBorder(),
-                          labelText: "Content",
-                          hintText: "Enter Note Content",
-                          prefixIcon: Icon(Icons.title),
-                          disabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(color: Colors.teal))),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                child: Column(children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                      padding: EdgeInsets.all(5),
+                      height: 400,
+                      width: double.infinity,
+                      child: Column(
                         children: [
-                          ElevatedButton(
-                              style: ButtonStyle(
-                                  fixedSize:
-                                      MaterialStateProperty.all(Size(100, 40)),
-                                  shape: MaterialStateProperty.all(
-                                      RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.vertical(
-                                              top: Radius.circular(5),
-                                              bottom: Radius.circular(20)))),
-                                  backgroundColor: MaterialStateProperty.all(
-                                      Colors.lightGreen)),
-                              onPressed: () {
-                                setState(() {
-                                  print("key");
-                                  addNote();
-                                });
-                              },
-                              child: Text(
-                                "Save",
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold),
-                              )),
-                          ElevatedButton(
-                              style: ButtonStyle(
-                                  fixedSize:
-                                      MaterialStateProperty.all(Size(100, 40)),
-                                  shape: MaterialStateProperty.all(
-                                      RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.vertical(
-                                              top: Radius.circular(5),
-                                              bottom: Radius.circular(20)))),
-                                  backgroundColor:
-                                      MaterialStateProperty.all(Colors.red)),
-                              onPressed: () {
-                                setState(() {
-                                  content.clear();
-                                  title.clear();
-                                });
-                              },
-                              child: Text(
-                                "Clear",
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold),
-                              ))
-                        ]),
-                  ],
-                )),
-          ]),
-        ));
+                          TextField(
+                            controller: title,
+                            key: _tkey,
+                            maxLines: 1,
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.teal),
+                                    borderRadius: BorderRadius.circular(10)),
+                                focusColor: Colors.teal,
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10)),
+                                border: OutlineInputBorder(),
+                                labelText: "Title",
+                                hintText: "Enter Title",
+                                prefixIcon: Icon(Icons.title),
+                                disabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide:
+                                        BorderSide(color: Colors.teal))),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextField(
+                            controller: content,
+                            key: _ckey,
+                            maxLines: 10,
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.teal),
+                                    borderRadius: BorderRadius.circular(10)),
+                                focusColor: Colors.teal,
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10)),
+                                border: OutlineInputBorder(),
+                                labelText: "Content",
+                                hintText: "Enter Note Content",
+                                prefixIcon: Icon(Icons.title),
+                                disabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide:
+                                        BorderSide(color: Colors.teal))),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                ElevatedButton(
+                                    style: ButtonStyle(
+                                        fixedSize: MaterialStateProperty.all(
+                                            Size(100, 40)),
+                                        shape: MaterialStateProperty.all(
+                                            RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.vertical(
+                                                        top: Radius.circular(5),
+                                                        bottom: Radius.circular(
+                                                            20)))),
+                                        backgroundColor:
+                                            MaterialStateProperty.all(
+                                                Colors.lightGreen)),
+                                    onPressed: () {
+                                      setState(() {
+                                        print("key");
+                                        addNote();
+                                      });
+                                    },
+                                    child: Text(
+                                      "Save",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                                ElevatedButton(
+                                    style: ButtonStyle(
+                                        fixedSize: MaterialStateProperty.all(
+                                            Size(100, 40)),
+                                        shape: MaterialStateProperty.all(
+                                            RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.vertical(
+                                                        top: Radius.circular(5),
+                                                        bottom: Radius.circular(
+                                                            20)))),
+                                        backgroundColor:
+                                            MaterialStateProperty.all(
+                                                Colors.red)),
+                                    onPressed: () {
+                                      setState(() {
+                                        content.clear();
+                                        title.clear();
+                                      });
+                                    },
+                                    child: Text(
+                                      "Clear",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ))
+                              ]),
+                        ],
+                      )),
+                ]),
+              ));
   }
 }
